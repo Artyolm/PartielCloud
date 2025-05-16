d72e437f32585a2783bc6e86d06cebc73db447d8
Artyolm <knakihuzuni@gmail.com>
2025-05-16 20:17:22 +0200

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const cors = require('cors');
 
 // Serve static files (if needed)
