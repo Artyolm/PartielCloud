e0cc366b23bf33b829e55ac0f1d13f3df56b18f2
Artyolm <knakihuzuni@gmail.com>
2025-05-16 23:24:29 +0200

    test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const cors = require("cors");
@@ -7,13 +8,22 @@ const mysql = require("mysql2/promise"); // Utilisation de la version promise
 // Middleware
 app.use(cors());
 app.use(express.json());
+dotenv.config();
 
 // Configuration de la base de données
-const dbConfig = {
+/*const dbConfig = {
   host: process.env["DB_HOST"],
   user: process.env["DB_USER"],
   password: process.env["DB_PASSWORD"],
   database: process.env["DB_NAME"],
+  ssl: { rejectUnauthorized: false } // Nécessaire pour Azure
+};*/
+
+const dbConfig = {
+  host: "db-partielcloud.database.windows.net",
+  user: "CloudSAfc354838@db-partielcloud",
+  password: "root8root?",
+  database: "db-partielcloud",
   ssl: { rejectUnauthorized: false } // Nécessaire pour Azure
 };
 
